dd95581a3001043188c406801332a51694801b1a
marinlagoutte-cmd <marin.lagoutte@gmail.com>
2026-08-14 22:18:12 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import { Oswald, Inter } from 'next/font/google';
 import '../styles/globals.css';
 
+const oswald = Oswald({ subsets: ['latin'], weight: ['600', '700'], variable: '--font-display' });
+const inter = Inter({ subsets: ['latin'], variable: '--font-body' });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${oswald.variable} ${inter.variable} font-body`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
